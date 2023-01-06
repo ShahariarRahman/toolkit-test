045597e61dd31ec4d5e2e0e8ffb010986f82b145
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-07 05:19:15 +0600

    createSlice for counterSlice

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    count: 0,
+};
+
+const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {},
+
+});
